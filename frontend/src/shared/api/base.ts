53681aeac243b49952416a0b49f34d177610d31d
vitaya-para <90479295+vitaya-para@users.noreply.github.com>
2025-11-12 16:08:47 +0300

    fix insert bearer token

--- frontend/src/shared/api/base.ts
+++ frontend/src/shared/api/base.ts
@@ -6,6 +6,13 @@ export const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:30
 // Создаем экземпляр axios
 export const apiClient = new Api({
     baseURL: API_BASE_URL,
+    securityWorker: () => {
+        return {
+            headers: {
+                Authorization: `Bearer ${tokenService.getAccessToken()}`,
+            },
+        };
+    },
 })
 
 // Интерфейс для токенов
